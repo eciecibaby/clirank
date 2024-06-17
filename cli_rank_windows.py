fe311c2b4fbca253dec05726eb6184541fcec46f
bananasniper <153735337+eciecibaby@users.noreply.github.com>
2024-06-17 02:12:02 -0400

    v1.1

---
+++ cli_rank_windows.py
@@ -0,0 +1,27 @@
+import os
+import sys
+import time
+import random
+
+running = True
+
+try:
+	while running:
+		os.system('cls')
+		print("CliRank | v1.1")
+		print("Rank 1 is highest, rank 6 is lowest")
+		print()
+		ttl = input("What are you ranking (Enter title)? ")
+		print()
+		rk1 = input("1. ")
+		rk2 = input("2. ")
+		rk3 = input("3. ")
+		rk4 = input("4. ")
+		rk5 = input("5. ")
+		rk6 = input("6. ")
+		print()
+		input("Press any key to exit...")
+		break
+		sys.exit()
+except KeyboardInterrupt:
+	sys.exit()
